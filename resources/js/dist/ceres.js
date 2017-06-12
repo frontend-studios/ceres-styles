f6ebb5170ff914602ed8a9cd0c5a390a224837ce
Jan Mense <janmense@googlemail.com>
2017-06-12 12:08:29 +0200

    update build files

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -14552,6 +14552,23 @@ Vue.filter("itemImage", function (item, baseUrl) {
 },{}],68:[function(require,module,exports){
 "use strict";
 
+Vue.filter("itemImages", function (images, accessor) {
+    var imageUrls = [];
+    var imagesAccessor = "all";
+
+    if (images.variation.length) {
+        imagesAccessor = "variation";
+    }
+
+    for (var i in images[imagesAccessor]) {
+        var imageUrl = images[imagesAccessor][i][accessor];
+
+        imageUrls.push({ url: imageUrl, position: images[imagesAccessor][i].position });
+    }
+
+    return imageUrls;
+});
+
 },{}],69:[function(require,module,exports){
 "use strict";
 
