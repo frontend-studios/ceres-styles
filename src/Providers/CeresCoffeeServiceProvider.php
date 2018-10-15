8d8dffd1f6728f71c5b081fceb52de8ba4a0d86e
Daniel Kaschlaw <d.kaschlaw@zentral-kommunikation.de>
2018-10-15 11:47:08 +0200

    Changing SP

--- src/Providers/CeresCoffeeServiceProvider.php
+++ src/Providers/CeresCoffeeServiceProvider.php
@@ -2,10 +2,15 @@
 
 namespace CeresCoffee\Providers;
 
+use IO\Extensions\Functions\Partial;
+use IO\Helper\CategoryKey;
+use IO\Helper\CategoryMap;
 use IO\Helper\TemplateContainer;
-use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ConfigRepository;
+use IO\Helper\ComponentContainer;
 
 class CeresCoffeeServiceProvider extends ServiceProvider
 {
@@ -15,16 +20,17 @@ class CeresCoffeeServiceProvider extends ServiceProvider
     public function register() {
 
     }
-    public function boot (Twig $twig, Dispatcher $eventDispatcher)
+    public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
     {
         // provide template to use for homepage
         $eventDispatcher->listen('IO.tpl.home', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("CeresCoffee::Homepage.Homepage");
             return false;
         });
+
         $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
-           $partial->set('page-design', 'CeresCoffee::PageDesign.PageDesign');
-       }, self::EVENT_LISTENER_PRIORITY);
+            $partial->set('page-design', 'CeresCoffee::PageDesign.PageDesign');
+        }, self::EVENT_LISTENER_PRIORITY););
 
     }
 }
