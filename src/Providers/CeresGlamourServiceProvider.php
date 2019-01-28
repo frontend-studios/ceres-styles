84ae25c94bce9039b344f037d668ee3e36209766
Daniel Kaschlaw <d.kaschlaw@zentral-kommunikation.de>
2019-01-28 11:12:57 +0100

    Changing SP

--- src/Providers/CeresGlamourServiceProvider.php
+++ src/Providers/CeresGlamourServiceProvider.php
@@ -2,39 +2,59 @@
 
 namespace CeresGlamour\Providers;
 
-use IO\Extensions\Functions\Partial;
-use IO\Helper\CategoryKey;
-use IO\Helper\CategoryMap;
-use IO\Helper\TemplateContainer;
+use Ceres\Caching\NavigationCacheSettings;
+use Ceres\Caching\SideNavigationCacheSettings;
+use IO\Services\ContentCaching\Services\Container;
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\Templates\Twig;
+use IO\Helper\TemplateContainer;
+use IO\Extensions\Functions\Partial;
+use IO\Services\ItemSearch\Helper\ResultFieldTemplate;
 use Plenty\Plugin\ConfigRepository;
-use IO\Helper\ComponentContainer;
 
+
+/**
+ * Class CeresGlamourServiceProvider
+ * @package CeresGlamour\Providers
+ */
 class CeresGlamourServiceProvider extends ServiceProvider
 {
-    const EVENT_LISTENER_PRIORITY = 99;
-    /**
-     * Register the service provider.
-     */
+    const PRIORITY = 0;
 
-    public function register() {
+    public function register()
+    {
 
     }
 
-    public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
+    public function boot(Twig $twig, Dispatcher $dispatcher, ConfigRepository $config)
     {
 
-        // provide template to use for homepage
-        $eventDispatcher->listen('IO.tpl.home', function(TemplateContainer $container, $templateData) {
-            $container->setTemplate("CeresGlamour::Homepage.Homepage");
+        $enabledOverrides = explode(", ", $config->get("CeresGlamour.templates.override"));
+
+        // Override partials
+        $dispatcher->listen('IO.init.templates', function (Partial $partial) use ($enabledOverrides)
+        {
+
+            $partial->set('page-design', 'Ceres::PageDesign.PageDesign');
+
+            if (in_array("page_design", $enabledOverrides) || in_array("all", $enabledOverrides))
+            {
+                $partial->set('page-design', 'CeresGlamour::PageDesign.PageDesign');
+            }
+
             return false;
-        });
+        }, self::PRIORITY);
 
-        $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
-            $partial->set('page-design', 'CeresGlamour::PageDesign.PageDesign');            
-        }, self::EVENT_LISTENER_PRIORITY);
+        // Override homepage
+        if (in_array("homepage", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
 
+            $dispatcher->listen('IO.tpl.home', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresGlamour::Homepage.Homepage');
+                return false;
+            }, self::PRIORITY);
+        }
     }
 }
