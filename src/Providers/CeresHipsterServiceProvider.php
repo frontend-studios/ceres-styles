e0efa72928b02384055e1fe47b3da60a6a8cf350
Daniel Kaschlaw <d.kaschlaw@zentral-kommunikation.de>
2019-02-13 08:50:02 +0100

    Update SP 3

--- src/Providers/CeresHipsterServiceProvider.php
+++ src/Providers/CeresHipsterServiceProvider.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace CeresHipster\Providers;
-
 use IO\Extensions\Functions\Partial;
 use IO\Helper\CategoryKey;
 use IO\Helper\CategoryMap;
@@ -11,34 +9,23 @@ use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ConfigRepository;
 use IO\Helper\ComponentContainer;
-
-
 class CeresHipsterServiceProvider extends ServiceProvider
 {
     const EVENT_LISTENER_PRIORITY = 99;
     /**
      * Register the service provider.
      */
-
     public function register() {
-
     }
-
     public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
     {
-
-        // provide template to use for homepage
         $eventDispatcher->listen('IO.tpl.home', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("CeresHipster::Homepage.Homepage");
             return false;
         });
-
-        $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
-        {
+        //partial replacement
+        $eventDispatcher->listen('IO.init.templates', function (Partial $partial) {
             $partial->set('page-design', 'CeresHipster::PageDesign.PageDesign');
-
         }, self::EVENT_LISTENER_PRIORITY);
-
-
     }
 }
